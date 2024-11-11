56a7b4a78ee68852cb7595dba349b3ffcec34735
MaxwellHinton <qwv9850@autuni.ac.nz>
2024-11-11 14:17:32 +1300

    4th try

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const port = process.env.PORT;  // Dynamically assign the port
-  await app.listen(port);
+  await app.listen(port || 8082);
 
   console.log(`Backend running on port: ${port}`);  // Log the correct port
 }
