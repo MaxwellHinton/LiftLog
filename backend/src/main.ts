ca1e5610d7401cc02207dde48a8f996d3c4f5f0a
MaxwellHinton <qwv9850@autuni.ac.nz>
2024-12-04 15:47:29 +1300

    Changed nestjs to log console.logs

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { logger: ['log', 'error', 'warn', 'debug', 'verbose']});
   const port = process.env.PORT || 8082;
 
   app.enableCors({
